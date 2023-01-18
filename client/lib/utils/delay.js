9c0f6679836618f20acbf63b0ba00093771ffaf3
Butter on Bread <qorbaxk97@gmail.com>
2023-01-18 10:28:42 +0900

    Promise 학습

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -15,3 +15,30 @@ delay(() => {
     });
   });
 });
+
+const delayP = (
+  shouldReject = false,
+  timeout = 1000,
+  errorMessage = "알 수 없는 오류가 발생했습니다.",
+  data = "성공했습니다."
+) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+    }, timeout);
+  });
+};
+
+delayP(false, 1000, "오류가 발생했다!").then((res) => {
+  console.log(res);
+});
+
+// delayP()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// console.log(delayP());
