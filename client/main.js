acf5c31ca8bd34be09b8cd3e83c7aa3a1ef25433
Butter on Bread <qorbaxk97@gmail.com>
2023-01-19 16:10:32 +0900

    스토리지에 저장되는 텍스트필드 만들기

--- client/main.js
+++ client/main.js
@@ -1,2 +1,16 @@
-console.log("이야이야요");
-console.log(window);
+import { getNode, loadStorage, saveStorage } from "./lib/index.js";
+
+const textField = getNode("#textField");
+const deleteButton = getNode('input[value="삭제"]');
+
+// 데이터가 계속 살아있을 수 있도록 
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+// textField에 받은 값을 storage에 저장
+const inputHandler = () => {
+  saveStorage("area", textField.value);
+};
+
+textField.addEventListener("input", inputHandler);
